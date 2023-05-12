878848e2357050babf3e866dcf0cc9d6291469a7
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-05-11 23:25:11 -0600

    Bullet impacts
    When a bullet collides with something it spawns impact particles and is removed. It also deletes the smoke trail particles though

---
+++ Scipts/basic_projectile.gd
@@ -0,0 +1,29 @@
+extends RigidBody3D
+
+@onready var impact_particles = preload("res://Prefabs/Particles/bullet_impact.tscn")
+@onready var current_scene = get_tree().get_root()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.contact_monitor = true
+	self.max_contacts_reported = 20
+	self.body_entered.connect(on_bullet_collision)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+
+func on_bullet_collision(node):
+	print("collision")
+	
+	place_impact()
+	queue_free()
+	
+	
+func place_impact():
+	var impact = impact_particles.instantiate()
+	current_scene.add_child(impact)
+	impact.set_global_position(get_global_position())
+	impact.emitting = true
