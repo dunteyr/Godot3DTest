c5dba6dc25f9f3caae8490c90cf419c6c467ee35
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-05-18 21:27:42 -0600

    Weapon Sway Attempt
    made the gun sway side to side when moving the camera. It breaks when I look down or up though. Im also using rotation to sway the gun but I might change it to translation

---
+++ Scipts/Gun.gd
@@ -0,0 +1,33 @@
+extends Node3D
+
+var mouse_mov
+@export var lerp_val = 5
+@export var left_sway_amt : float
+@export var right_sway_amt : float
+@export var sway_threshold = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_mov = -event.relative.x
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if mouse_mov != null:
+		if mouse_mov > sway_threshold:
+			rotation = rotation.lerp(Vector3(rotation.x,-left_sway_amt, rotation.z), lerp_val * delta)
+		elif mouse_mov < -sway_threshold:
+			rotation = rotation.lerp(Vector3(rotation.x,right_sway_amt, rotation.z), lerp_val * delta)
+		else:
+			rotation = rotation.lerp(Vector3(rotation.x,0.0,rotation.z), lerp_val * delta)
+			
+			
+	
+
+
