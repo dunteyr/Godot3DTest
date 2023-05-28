2484364892591b2c44b2e46b85ea4b544a5983b3
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-05-27 18:25:29 -0600

    Bullet holes work
    Bullet holes work now but have uncovered a bunch of problems with the bullet projectile collisions. Basically, even with constant collision detection, bullets pass through things that are really close to the player. I think some games just ray cast when shooting something very close and shoot a projectile for further distances. I might try doing that.

--- Scipts/basic_projectile.gd
+++ Scipts/basic_projectile.gd
@@ -2,7 +2,11 @@ extends RigidBody3D
 
 @export var max_lifetime = 3.0
 var current_lifetime = 0.0
+var impact_position
+var impact_direction
 
+@onready var impact_raycast : RayCast3D = get_node("Decal_Cast")
+@onready var bullet_hole_decal = preload("res://Prefabs/bullet_hole.tscn")
 @onready var impact_particles = preload("res://Prefabs/Particles/bullet_impact.tscn")
 @onready var current_scene = get_tree().get_root()
 
@@ -23,15 +27,45 @@ func _process(delta):
 	
 
 func on_bullet_collision(node):
-	
-	
+	impact_position = global_position
 	
 	place_impact()
 	queue_free()
 	
 	
 func place_impact():
+	
+	place_bullet_hole()
+	
 	var impact = impact_particles.instantiate()
 	current_scene.add_child(impact)
 	impact.set_global_position(get_global_position())
 	impact.emitting = true
+
+
+func get_impact_normal():
+	
+	if impact_raycast.is_colliding():
+		var normal = impact_raycast.get_collision_normal()
+		return normal
+	else:
+		print("No Collision")
+		return Vector3(0,0,0)
+
+
+func place_bullet_hole():
+	var impact_normal = get_impact_normal()	
+	print(impact_normal)
+	
+	var bullet_hole = bullet_hole_decal.instantiate()
+	current_scene.add_child(bullet_hole)
+	bullet_hole.set_global_position(get_global_position())
+	
+	#for some reason Node3D look_at() function fails if it needs to look straight up or straight down
+	#so in those cases just rotate the bullet hole manually
+	if impact_normal == Vector3.UP:
+		bullet_hole.global_rotation_degrees.x -= 90
+	elif impact_normal == Vector3.DOWN:
+		bullet_hole.global_rotation_degrees.x += 90
+	else:
+		bullet_hole.look_at(get_global_position() - impact_normal)
