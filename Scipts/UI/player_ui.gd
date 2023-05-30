184d091d53a1c6e3b5ff59f904e981c0da82f2ca
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-05-29 22:03:39 -0600

    UI updates with clip remaining
    The UI correctly displays number of shots left. There is no code for hitting zero and reloading though so for now it just goes negative and keeps firing

--- Scipts/UI/player_ui.gd
+++ Scipts/UI/player_ui.gd
@@ -1,11 +1,24 @@
 extends Control
 
+@onready var player = get_parent()
+@onready var mag_size_label = get_node("Ammo_Container/Mag_Size_Label")
+@onready var mag_remaining_label = get_node("Ammo_Container/Mag_Remaining_Label")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player.bullet_fired.connect(update_ammo_label)
+	init_ammo_label(player.magazine_size)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func update_ammo_label(new_label):
+	mag_remaining_label.set_text(str(new_label))
+
+
+func init_ammo_label(mag_size):
+	mag_size_label.set_text(str(mag_size))
+	mag_remaining_label.set_text(str(mag_size))
