46873d5c77d369de74a8d1b6aee9532330599f42
dunteyr <39315568+dunteyr@users.noreply.github.com>
2023-06-02 22:01:31 -0600

    Doing some tests
    I made the foot the only active bone to get an idea of how the joints work. I made it so that the mesh doesn't ragdoll until I press E

--- Scipts/Test Scripts/Character Mesh.gd
+++ Scipts/Test Scripts/Character Mesh.gd
@@ -1,23 +1,30 @@
 extends Node3D
 
-@export var ragdoll = false
+var ragdoll = false
 
 @onready var animation : AnimationPlayer = get_node("AnimationPlayer")
 @onready var skeleton : Skeleton3D = get_node("Armature/Skeleton3D")
+@onready var mesh : MeshInstance3D = get_node("Armature/Skeleton3D/Character_Cube002")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	if ragdoll:
-		enable_ragdoll()
-	else:		
-		animation.play("Low Poly Character/Low Poly Idle")
+	animation.play("Low Poly Character/Low Poly T Pose")	
+	#animation.play("Low Poly Character/Low Poly Idle")
+	mesh.set_visible(true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("Interact"):
+		#animation.play("Low Poly Character/Low Poly T Pose")
+		enable_ragdoll()
 
 
 func enable_ragdoll():
-	skeleton.physical_bones_start_simulation()
+	var active_bones : Array
+	var right_foot = skeleton.find_bone("mixamorig_RightFoot")
+	
+	active_bones.push_back(skeleton.get_bone_name(right_foot))
+	
+	skeleton.physical_bones_start_simulation(active_bones)
+	
